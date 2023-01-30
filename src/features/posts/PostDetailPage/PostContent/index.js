2944b929460f80a1aad7bed299984c5a61a95193
Imagemaker <imagemaker@IMCL-GXML7J.local>
2023-01-30 17:17:50 -0300

    Replace posts details styles with chakra

--- src/features/posts/PostDetailPage/PostContent/index.js
+++ src/features/posts/PostDetailPage/PostContent/index.js
@@ -1,42 +1,38 @@
 import ReactMarkdown from "react-markdown";
-import classes from "./PostContent.module.css";
 import PostHeader from "../PostHeader";
-import Image from "next/legacy/image";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Box, Text } from "@chakra-ui/react";
+import CustomImage from "../../../../shared/components/CustomImage";
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    // img(image) {
-    //   return (
-    //     <Image
-    //       src={`/images/posts/${post.slug}/${image.src}`}
-    //       alt={image.alt}
-    //       width={600}
-    //       height={300}
-    //       layout="responsive"
-    //     />
-    //   );
-    // },
+    h2(title) {
+      return (
+        <Text fontWeight="bold" marginY={4} fontSize="2xl">
+          {title.children}
+        </Text>
+      );
+    },
     p(paragraph) {
       const { node } = paragraph;
       if (node.children[0].tagName === "img") {
         const image = node.children[0];
         return (
-          <div className={classes.image}>
-            <Image
+          <Box marginBottom={2}>
+            <CustomImage
               src={`/images/posts/${post.slug}/${image.properties.src}`}
               alt={image.alt}
               width={600}
               height={300}
               layout="responsive"
             />
-          </div>
+          </Box>
         );
       }
-      return <p>{paragraph.children}</p>;
+      return <Text marginBottom={2}>{paragraph.children}</Text>;
     },
     code(code) {
       const { className, children } = code;
@@ -52,11 +48,24 @@ const PostContent = ({ post }) => {
   };
 
   return (
-    <article className={classes.content}>
+    <Box
+      width="95%"
+      maxWidth="60rem"
+      marginY={5}
+      marginX="auto"
+      fontSize="xl"
+      backgroundColor="gray.200"
+      borderRadius="6px"
+      padding={8}
+    >
       <PostHeader title={post.title} image={imagePath} />
       <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
-    </article>
+    </Box>
   );
 };
 
 export default PostContent;
+
+// margin: var(--size-4) auto;
+// width: 100%;
+// max-width: 600px;
--- src/features/posts/PostDetailPage/PostHeader/index.js
+++ src/features/posts/PostDetailPage/PostHeader/index.js
@@ -1,12 +1,22 @@
-import Image from "next/legacy/image";
-import classes from "./PostHeader.module.css";
+import { Flex, Heading } from "@chakra-ui/react";
+import CustomImage from "../../../../shared/components/CustomImage";
 
 const PostHeader = ({ title, image }) => {
   return (
-    <header className={classes.header}>
-      <h1>{title}</h1>
-      <Image src={image} alt={title} width={200} height={150} />
-    </header>
+    <Flex
+      pb={8}
+      borderBottom="8px solid"
+      borderColor="gray.500"
+      my={5}
+      flexDir={["column-reverse", "row"]}
+      justifyContent="space-between"
+      alignItems={["center", "flex-end"]}
+      textAlign={["center", "left"]}
+      gap="1rem"
+    >
+      <Heading size={["xl", "xl", "3xl"]}>{title}</Heading>
+      <CustomImage src={image} alt={title} width={200} height={150} />
+    </Flex>
   );
 };
 
